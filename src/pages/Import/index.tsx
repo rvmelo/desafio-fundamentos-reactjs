66340922137ed3d3e90f4418524d6af955f9ba57
rvmelo <robertomelo7@hotmail.com>
2020-11-23 15:22:24 -0300

    feat: add navigation and import features

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.forEach(file => {
+      setUploadedFiles((prev: FileProps[]) => {
+        return [
+          ...prev,
+          {
+            file,
+            name: file.name,
+            readableSize: filesize(file.size),
+          },
+        ];
+      });
+    });
   }
 
   return (
